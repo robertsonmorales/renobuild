21bff20bcd6430ddbda17cd402f68605b5a0c272
robertsonmorales <58919933+robertsonmorales@users.noreply.github.com>
2023-10-14 08:59:40 +0000

    updated responsiveness

--- src/components/Subheadline.jsx
+++ src/components/Subheadline.jsx
@@ -4,9 +4,9 @@ function Subheadline({ subheadline, description, theme = 'light', children }) {
     return (
         <div className="flex justify-center items-center flex-col">
             <h3 className={ (theme === 'dark' ? "text-white " : "") + "text-4xl text-center font-bold mb-4"  } dangerouslySetInnerHTML={{ __html: subheadline }} />
-            <p className={ (theme === 'dark' ? "text-neutral-200 " : "text-neutral-600 ") + "text-[18px] text-center leading-6 w-full lg:w-7/12" }>
+            <p className={ (theme === 'dark' ? "text-neutral-200 " : "text-neutral-600 ") + "text-[18px] text-center leading-6 w-full lg:w-7/12 flex justify-center" }>
                 <span>{ description }</span>
-                <span className="ml-2 hidden md:inline">{ children }</span>
+                <span className="ml-2 hidden md:inline-block">{ children }</span>
             </p>
         </div>
     );
