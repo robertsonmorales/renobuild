2709784c2326e7788a57c886b69210cbc17d42b7
Robertson Morales <moralesso152842@gmail.com>
2023-10-25 18:12:06 +0800

    update preloader

--- src/components/ui/Preloader.jsx
+++ src/components/ui/Preloader.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Preloader = () => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-900 text-white z-50">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-white mr-2"></div>
+    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-sky-100/50 text-neutral-700 z-50">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-amber-700 mr-2"></div>
       <p className="mt-4 lg:mt-0 text-lg">Loading...</p>
     </div>
   );
